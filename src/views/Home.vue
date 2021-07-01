e5c6c19fce399283cadddff013b127609ba26e27
Oriana Sanabria <orianasanabria2@gmail.com>
2021-06-30 21:58:13 -0400

    added gsap animations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,42 +2,51 @@
   <div class="home">
     <b-container>
       <!-- Header | Start -->
+
       <header>
         <b-row class="header-row g-0 d-flex align-items-center m-0 p-0">
           <b-col cols="12" lg="6">
-            <div class="info mb-5">
-              <p class="subtitle text-uppercase">All New | Visum</p>
-              <h1 class="text-uppercase my-2">Smart Watches</h1>
-              <p>El futuro de la salud está en tu muñeca.</p>
-              <p class="my-4 price">Desde $49.990</p>
-              <b-button
-                href="#categories"
-                class="btn btn-size btn-rounded btn-outlined me-3"
-                >Conoce Más</b-button
-              >
-              <b-button
-                :to="`/smartwatches`"
-                class="btn btn-size btn-rounded btn-accent"
-                >Ver Todos</b-button
-              >
-            </div>
-            <div
-              class="detail-wrapper px-5 d-flex align-items-center justify-content-end"
-            >
-              <div class="detail mx-5">
-                <h3 class="detail__title">Apple Pay</h3>
-                <p class="detail__info">Haz pagos seguros solo con tu reloj.</p>
+            <transition appear @before-enter="beforeEnter1" @enter="enter">
+              <div class="info mb-5">
+                <p class="subtitle text-uppercase">All New | Visum</p>
+                <h1 class="text-uppercase my-2">Smart Watches</h1>
+                <p>El futuro de la salud está en tu muñeca.</p>
+                <p class="my-4 price">Desde $49.990</p>
+                <b-button
+                  href="#categories"
+                  class="btn btn-size btn-rounded btn-outlined me-3"
+                  >Conoce Más</b-button
+                >
+                <b-button
+                  :to="`/smartwatches`"
+                  class="btn btn-size btn-rounded btn-accent"
+                  >Ver Todos</b-button
+                >
               </div>
-              <div class="detail">
-                <h3 class="detail__title">EGC2</h3>
-                <p class="detail__info">
-                  Un electro en tu muñeca en todo momento.
-                </p>
+            </transition>
+            <transition appear @before-enter="beforeEnter1" @enter="enter">
+              <div
+                class="detail-wrapper px-5 d-flex align-items-center justify-content-end"
+              >
+                <div class="detail mx-5">
+                  <h3 class="detail__title">Apple Pay</h3>
+                  <p class="detail__info">
+                    Haz pagos seguros solo con tu reloj.
+                  </p>
+                </div>
+                <div class="detail">
+                  <h3 class="detail__title">EGC2</h3>
+                  <p class="detail__info">
+                    Un electro en tu muñeca en todo momento.
+                  </p>
+                </div>
               </div>
-            </div>
+            </transition>
           </b-col>
           <b-col cols="12" lg="6" class="align-items-center">
-            <b-img src="img/watch-bg2.png" class=" hero-image pb-5" />
+            <transition appear @before-enter="beforeEnter2" @enter="enter">
+              <b-img src="img/watch-bg2.png" class="hero-image pb-5" />
+            </transition>
           </b-col>
         </b-row>
       </header>
@@ -123,6 +132,7 @@
 import { mapMutations, mapState } from "vuex";
 import MainCard from "@/components/MainCard.vue";
 import MainBanner from "@/components/MainBanner.vue";
+import gsap from "gsap";
 export default {
   name: "Home",
   components: {
@@ -130,6 +140,33 @@ export default {
     MainBanner,
   },
   methods: {
+    beforeEnter1(el) {
+      el.style.opacity = 0;
+      el.style.transform = "translateX(-100px)";
+    },
+    beforeEnter2(el) {
+      el.style.opacity = 0;
+      el.style.transform = "translateX(100px)";
+    },
+    enter() {
+      gsap.to(".info", {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+      });
+      gsap.to(".hero-image", {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        delay: 0.5,
+      });
+      gsap.to(".detail-wrapper", {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        delay: 1,
+      });
+    },
     scrollUp() {
       window.scrollTo(0, 0);
     },
